dc40095577b6744e6b8cbd6b45dfade7f48f3b14
Javier Seixas <dev@javierseixas.com>
2018-06-06 22:50:04 -0400

    Starts with graphene schema

--- app.py
+++ app.py
@@ -1,11 +1,57 @@
 from flask import Flask
+import graphene, json
+
+
+class Section(graphene.ObjectType):
+    text = graphene.String()
+
+    def resolve_text(self, info):
+        return self.text
+
+
+class Guide(graphene.ObjectType):
+    sections = graphene.List(lambda: Section)
+    name = graphene.String()
+
+    def resolve_name(self, info):
+        return 'Guide'
+
+    def resolve_sections(self, info):
+        return [Section(text='uno'), Section(text='dos')]
+
+
+class Query(graphene.ObjectType):
+    hello = graphene.String()
+    guides = graphene.List(lambda: Guide)
+
+    def resolve_hello(self, info):
+        return 'World'
+
+    def resolve_guides(self, info):
+        return [Guide()]
+
+
+schema = graphene.Schema(query=Query)
+
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/graphql')
 def hello_world():
-    return 'Hello World!'
+    result = schema.execute('''
+  query {
+    hello
+    guides {
+      name
+      sections {
+        text
+      }
+    }
+  }
+''')
+
+    return json.dumps(result.data, indent=4)
 
 
 if __name__ == '__main__':
